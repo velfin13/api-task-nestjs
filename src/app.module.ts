1c8dfd5e2d197ab5b9dbf14e736151852e52a354
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-08-09 11:25:33 -0500

    create module, controller, interface, dto belowns task

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskController } from './task/task.controller';
+import { TaskService } from './task/task.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/task/task.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('api/v1/task')
+export class TaskController {}
