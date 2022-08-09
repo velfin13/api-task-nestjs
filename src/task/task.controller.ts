afb2761a6ed981a6a9c6cf4fdef84bbf621df4f5
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-08-09 11:33:43 -0500

    method get

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('api/v1/task')
 export class TaskController {
   @Post()
   method(@Req() req: Request) {
+    return `method ${req.method}`;
+  }
+
+  @Get()
+  methodGet(@Req() req: Request) {
     return `method ${req.method}`;
   }
 }
