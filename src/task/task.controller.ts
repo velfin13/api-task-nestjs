18083c16dd895398014062e48e4dc4c40d6e6e60
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-08-09 11:36:56 -0500

    method put path

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Patch, Post, Put, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('api/v1/task')
@@ -10,6 +10,16 @@ export class TaskController {
 
   @Get()
   methodGet(@Req() req: Request) {
+    return `method ${req.method}`;
+  }
+
+  @Put()
+  methodPut(@Req() req: Request) {
+    return `method ${req.method}`;
+  }
+
+  @Patch()
+  methodPath(@Req() req: Request) {
     return `method ${req.method}`;
   }
 }
