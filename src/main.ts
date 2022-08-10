02ad911b9d2196d4879dfab232959f85f39360c1
Velfin Velasquez <velfinvelasquez@gmail.com>
2022-08-10 07:51:25 -0500

    manejo de interceptores globales

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filtters/http-exeption.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
   await app.listen(3000);
 }
 bootstrap();
